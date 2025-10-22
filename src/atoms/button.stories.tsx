d205044730d187d941d3d065682cc4b76d562436
Kyle Knight <122591+kyleknighted@users.noreply.github.com>
2025-10-22 15:59:49 -0500

    Update icon

--- src/atoms/button.stories.tsx
+++ src/atoms/button.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { AcornIcon } from '@phosphor-icons/react';
+import { DownloadIcon, AcornIcon } from '@phosphor-icons/react';
 import 'frosted-ui/styles.css';
 import { Code, Spinner, Text } from 'frosted-ui';
 
@@ -18,16 +18,6 @@ const buttonPropDefs = {
   },
 } as const;
 
-const ExampleIcon = () => (
-  <svg width="16" height="16" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path
-      d="M3 2.5C3 2.22386 3.22386 2 3.5 2H11.5C11.7761 2 12 2.22386 12 2.5V13.5C12 13.6818 11.9014 13.8492 11.7424 13.9373C11.5834 14.0254 11.3891 14.0203 11.235 13.924L7.5 11.5896L3.765 13.924C3.61087 14.0203 3.41659 14.0254 3.25762 13.9373C3.09864 13.8492 3 13.6818 3 13.5V2.5ZM4 3V12.5979L6.97 10.7416C7.29427 10.539 7.70573 10.539 8.03 10.7416L11 12.5979V3H4Z"
-      fill="currentColor"
-      fillRule="evenodd"
-      clipRule="evenodd"
-    ></path>
-  </svg>
-);
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
   title: 'Atoms/Button',
@@ -161,7 +151,7 @@ export const WithIcons: Story = {
   render: (args: ButtonProps) => (
     <div style={{ display: 'flex', alignItems: 'center', gap: 'var(--space-2)' }}>
       <Button {...args}>
-        <ExampleIcon /> With icon
+        <AcornIcon /> With icon
       </Button>
     </div>
   ),
@@ -194,13 +184,13 @@ export const Loading: Story = {
       <div style={{ display: 'flex', alignItems: 'center', gap: 'var(--space-4)' }}>
         <Button {...args} loading={false} variant="classic">
           <Spinner loading={false}>
-            <AcornIcon />
+            <DownloadIcon />
           </Spinner>
           Download
         </Button>
         <Button {...args} loading={false} variant="classic" disabled>
           <Spinner loading>
-            <AcornIcon />
+            <DownloadIcon />
           </Spinner>
           Download
         </Button>
@@ -208,13 +198,13 @@ export const Loading: Story = {
       <div style={{ display: 'flex', alignItems: 'center', gap: 'var(--space-4)' }}>
         <Button {...args} loading={false} variant="soft">
           <Spinner loading={false}>
-            <AcornIcon />
+            <DownloadIcon />
           </Spinner>
           Download
         </Button>
         <Button {...args} loading={false} variant="soft" disabled>
           <Spinner loading>
-            <AcornIcon />
+            <DownloadIcon />
           </Spinner>
           Download
         </Button>
@@ -222,13 +212,13 @@ export const Loading: Story = {
       <div style={{ display: 'flex', alignItems: 'center', gap: 'var(--space-4)' }}>
         <Button {...args} loading={false} variant="solid">
           <Spinner loading={false}>
-            <AcornIcon />
+            <DownloadIcon />
           </Spinner>
           Download
         </Button>
         <Button {...args} loading={false} variant="solid" disabled>
           <Spinner loading>
-            <AcornIcon />
+            <DownloadIcon />
           </Spinner>
           Download
         </Button>
@@ -236,13 +226,13 @@ export const Loading: Story = {
       <div style={{ display: 'flex', alignItems: 'center', gap: 'var(--space-4)' }}>
         <Button {...args} loading={false} variant="surface">
           <Spinner loading={false}>
-            <AcornIcon />
+            <DownloadIcon />
           </Spinner>
           Download
         </Button>
         <Button {...args} loading={false} variant="surface" disabled>
           <Spinner loading>
-            <AcornIcon />
+            <DownloadIcon />
           </Spinner>
           Download
         </Button>

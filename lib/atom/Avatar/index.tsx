777b525870a765d5fa6cf0ecb4ef4ac3be3e1a98
Kyle Knight <122591+kyleknighted@users.noreply.github.com>
2025-10-30 09:11:12 -0500

    Adding Avatar component & stories

---
+++ lib/atom/Avatar/index.tsx
@@ -0,0 +1,8 @@
+import { Avatar as FrostedAvatar } from 'frosted-ui';
+import type { IAvatarProps } from './types';
+
+const Avatar = ({ name, pfp }: IAvatarProps) => {
+  return <FrostedAvatar shape="circle" color="blue" size="3" fallback={name} src={pfp} />;
+};
+
+export default Avatar;

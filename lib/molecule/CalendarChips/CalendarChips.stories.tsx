9e4867a437fdd1b69b3b76ac0c6775cd26b00d0c
Kyle Knight <122591+kyleknighted@users.noreply.github.com>
2025-10-31 11:51:05 -0500

    Adding CalendarChips

---
+++ lib/molecule/CalendarChips/CalendarChips.stories.tsx
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { CalendarChips, type ICalendarChipsProps } from '../..';
+
+const defaultArgs: ICalendarChipsProps = {
+  numberDaysShown: 7,
+  selectedDate: new Date(),
+};
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta = {
+  title: 'Molecule/CalendarChips',
+  component: CalendarChips,
+  parameters: {
+    layout: 'centered',
+  },
+  args: { ...defaultArgs },
+  tags: ['autodocs'],
+} satisfies Meta<ICalendarChipsProps>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    ...defaultArgs,
+  },
+};
--- lib/molecule/index.ts
+++ lib/molecule/index.ts
@@ -1,3 +1,5 @@
 export { default as Menu, type IMenuProps } from './Menu';
 
 export { default as Breadcrumbs, type IBreadcrumbsProps } from './Breadcrumbs';
+
+export { default as CalendarChips, type ICalendarChipsProps } from './CalendarChips';

2fb3e55b4418407ca78bf2c7ee13c59abda8dca3
Kyle Knight <122591+kyleknighted@users.noreply.github.com>
2025-11-03 12:22:00 -0600

    Calendar chips date math (#16)
    * updating math for the selectedDate
    
    * CalChips rendering update

--- lib/molecule/CalendarChips/types.ts
+++ lib/molecule/CalendarChips/types.ts
@@ -1,6 +1,8 @@
 export interface ICalendarChipsProps {
   /* The date to be selected */
-  readonly selectedDate: Date;
+  readonly selectedDate?: Date;
+  /* The end date for the range of dates to show */
+  readonly endDate?: Date;
   /* Number of chips to show including the selected date chip */
   readonly numberDaysShown?: 1 | 3 | 5 | 7;
 }

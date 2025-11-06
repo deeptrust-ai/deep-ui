4c5ed3dd02c711d7fc18554d095564c5a401935e
Kyle Knight <122591+kyleknighted@users.noreply.github.com>
2025-11-05 22:54:34 -0600

    header bg color

--- lib/compound/Table/index.tsx
+++ lib/compound/Table/index.tsx
@@ -24,7 +24,7 @@ const Table = (props: ITableProps) => {
         <FrostedScrollArea scrollbars="horizontal">
           <FrostedTable.Table>
             <FrostedTable.Header>
-              <FrostedTable.Row>
+              <FrostedTable.Row className={styles.headerRow}>
                 <FrostedTable.ColumnHeaderCell>Event</FrostedTable.ColumnHeaderCell>
                 <FrostedTable.ColumnHeaderCell>Severity</FrostedTable.ColumnHeaderCell>
                 <FrostedTable.ColumnHeaderCell>Date</FrostedTable.ColumnHeaderCell>

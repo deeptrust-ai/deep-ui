4992187f14e8075b6bdbd884ba287010e284a3d3
Kyle Knight <122591+kyleknighted@users.noreply.github.com>
2025-11-07 22:17:58 -0600

    updating export

--- lib/index.ts
+++ lib/index.ts
@@ -2,3 +2,6 @@ export * from './atom';
 export * from './molecule';
 export * from './compound';
 export * from './layout';
+
+// Re-export Radix UI themes components for convenience
+export { Box, Flex, Grid, Container, Section } from '@radix-ui/themes';

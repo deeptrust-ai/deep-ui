d8234476df8d79666a78b5eeb6dc63480fea9c8e
Kyle Knight <122591+kyleknighted@users.noreply.github.com>
2025-12-06 17:11:21 -0600

    spacing around heading

--- lib/atom/ContentWrapper/ContentWrapper.tsx
+++ lib/atom/ContentWrapper/ContentWrapper.tsx
@@ -1,4 +1,4 @@
-import { Box, Container } from '@radix-ui/themes';
+import { Box, Flex } from '@radix-ui/themes';
 import { Heading } from 'frosted-ui';
 import styles from './ContentWrapper.styles.module.css';
 import type { IContentWrapperProps } from './ContentWrapper.types';
@@ -6,18 +6,20 @@ import Header from '../Header';
 
 const ContentWrapper = ({ children, title, subtitle, metaInfo, sidebar }: IContentWrapperProps) => {
   return (
-    <Container className={styles.wrapper}>
+    <Box className={styles.wrapper}>
       {sidebar ? (
-        <Heading as="h3" weight="bold" size="3">
-          {title}
-        </Heading>
+        <Flex direction="row" justify="between" px="5" pt="4">
+          <Heading as="h3" weight="bold" size="4">
+            {title}
+          </Heading>
+        </Flex>
       ) : (
         <Header title={title} subtitle={subtitle} metaInfo={metaInfo} />
       )}
       <Box py="4" px="5">
         {children}
       </Box>
-    </Container>
+    </Box>
   );
 };
 

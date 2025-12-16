57923134ab083d905b2444d603f07c59fdcd566e
Kyle Knight <122591+kyleknighted@users.noreply.github.com>
2025-12-15 20:07:01 -0600

    severity text colors

--- lib/atom/index.ts
+++ lib/atom/index.ts
@@ -14,3 +14,4 @@ export { default as Header, type IHeaderProps } from './Header';
 export { default as User, type IUserProps } from './User';
 export { default as Toast, type IToastProps } from './Toast';
 export { default as ContentWrapper, type IContentWrapperProps } from './ContentWrapper';
+export { default as Severity, type ISeverityProps } from './Severity/Severity';

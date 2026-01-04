bbbbb7f723a5021d7b667b127659d0960fbbe707
Kyle Knight <122591+kyleknighted@users.noreply.github.com>
2026-01-03 18:20:26 -0600

    Fixing up eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,10 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import { defineConfig, globalIgnores } from 'eslint/config';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const tsconfigRootDir = path.dirname(fileURLToPath(import.meta.url));
 
 const tsConfigs = Array.isArray(tseslint.configs.recommended)
   ? tseslint.configs.recommended
@@ -25,6 +29,9 @@ export default defineConfig([
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        tsconfigRootDir,
+      },
     },
   },
 ]);

dcb9389b66bd6c6d67980e30ea955ae27e5d6d42
Kyle Knight <122591+kyleknighted@users.noreply.github.com>
2026-02-22 21:05:40 -0600

    remove unnecessary files

--- lib/compound/InboxView/parts/RiskAnalysis.tsx
+++
@@ -1,7 +0,0 @@
-import { Tabs } from '@radix-ui/themes';
-
-const RiskAnalysis = () => {
-  return <Tabs.Content value="risk-analysis">Risk Analysis content</Tabs.Content>;
-};
-
-export default RiskAnalysis;
--- lib/compound/InboxView/parts/Users.tsx
+++
@@ -1,8 +0,0 @@
-import { Tabs } from '@radix-ui/themes';
-import React from 'react';
-
-const Users = () => {
-  return <Tabs.Content value="users">Users content</Tabs.Content>;
-};
-
-export default Users;

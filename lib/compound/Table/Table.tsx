b45d8d16114e3930d0b00689da3b756101851e71
Kyle Knight <122591+kyleknighted@users.noreply.github.com>
2026-03-25 19:32:35 -0500

    fixing Table.tsx action deprecation

--- lib/compound/Table/Table.tsx
+++ lib/compound/Table/Table.tsx
@@ -48,10 +48,7 @@ const Table = ({
       }),
     [headers.length, rows]
   );
-  const showActionsColumn = useMemo(
-    () => validRows.some((row) => row.actions && row.actions.length > 0),
-    [validRows]
-  );
+
   const rowsPerPage = validRows.slice(startIndex, endIndex);
 
   return (
@@ -62,14 +59,13 @@ const Table = ({
             {headers.map((header) => (
               <RadixTable.ColumnHeaderCell key={`${header}`}>{header}</RadixTable.ColumnHeaderCell>
             ))}
-            {showActionsColumn ? <RadixTable.ColumnHeaderCell /> : null}
           </RadixTable.Row>
         </RadixTable.Header>
         <RadixTable.Body>
           {rowsPerPage.length ? (
             rowsPerPage.map((row) => <TableRow key={row.id} {...row} />)
           ) : (
-            <TableEmpty colSpan={headers.length + (showActionsColumn ? 1 : 0)} />
+            <TableEmpty colSpan={headers.length} />
           )}
         </RadixTable.Body>
       </RadixTable.Root>

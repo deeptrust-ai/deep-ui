fff78d885f783e839e233ae43aad59c542517427
Kyle Knight <122591+kyleknighted@users.noreply.github.com>
2026-04-09 12:29:25 -0500

    updating stories and types

--- lib/atom/Badge/types.ts
+++ lib/atom/Badge/types.ts
@@ -1,7 +1,4 @@
 import type { BadgeProps } from '@radix-ui/themes';
 
 /** Props for the {@link Badge} atom component. */
-export interface IBadgeProps extends BadgeProps {
-  /** The text label to be displayed inside the badge */
-  label: string;
-}
+export type IBadgeProps = BadgeProps;
